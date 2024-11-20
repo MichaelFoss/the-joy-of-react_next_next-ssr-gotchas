9bb69ec86647e055ec1d12fe0e506c07ad1a71e5
Michael Foss <michael.foss@mdpanel.com>
2024-11-20 04:46:04 -0500

    Use Spinner placeholder

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,14 +1,13 @@
 'use client';
 import React, { useEffect } from 'react';
+import Spinner from '../Spinner';
 
 function Counter() {
-  const [count, setCount] = React.useState();
+  const [count, setCount] = React.useState(null);
 
   useEffect(() => {
     const savedValue = Number(window.localStorage.getItem('saved-count'));
-    if (!!savedValue) {
-      setCount(savedValue);
-    }
+    setCount(savedValue || 0);
   }, []);
 
   useEffect(() => {
@@ -17,7 +16,7 @@ function Counter() {
 
   return (
     <button className="count-btn" onClick={() => setCount(count + 1)}>
-      Count: {count}
+      Count: {count === null ? <Spinner /> : count}
     </button>
   );
 }
